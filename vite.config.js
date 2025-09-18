fbb5ae8cef35c850ed9a5ecfbd4b122da3a984dd
David Olivetto <dxdavidex63@gmail.com>
2025-09-17 20:24:14 -0600

    Actualización de estilos y mejoras en la app

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/collection-app/' // Muy importante para GitHub Pages
+})
